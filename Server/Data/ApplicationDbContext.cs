b010605ebb6599c8233ab3a6c5a05c7efb86fcb3
Omar Rodríguez <alebritje@outlook.com>
2023-09-19 18:10:20 -0600

    EFCore

---
+++ Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PizzaBlazor.Shared.Models;
+
+namespace PizzaBlazor.Server.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Ingrediente> Ingredientes { get; set; }
+        public DbSet<Tamanos> Tamanos { get; set; }
+    }
+}
